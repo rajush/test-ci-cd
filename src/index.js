33b7ec3ea5a38e647be5d7ac7ea6555ba5f3f6b2
rajush <rajushshakya@gmail.com>
2019-03-31 14:26:17 -0700

    - test commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('./style.css');
 require('./modules/jsChecker');
 
 if (ENVIRONMENT === 'production') {
-  console.info('Running app in the production environment!');
+  console.info('Running app in the production environment!!');
 } else {
   console.info('Running app in the staging environment!');
 }
